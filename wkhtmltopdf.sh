e19ce1cec2691c654e2cc0fb27cdd4f0c37f70de
Swilder-M <i@codming.com>
2022-11-11 20:23:01 +0800

    fix sudo

--- wkhtmltopdf.sh
+++ wkhtmltopdf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf_bin -q $*
+xvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/local/bin/wkhtmltopdf_bin -q $*
